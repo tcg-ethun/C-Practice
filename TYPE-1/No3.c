979c81a4777fea613765b13c54510124816781dd
Esan <ethuntcg@gmail.com>
2025-05-03 17:33:15 +0600

    Updated New

---
+++ TYPE-1/No3.c
@@ -0,0 +1,18 @@
+
+//Count the Number of Digits//
+
+#include<stdio.h>
+int main()
+{
+   int  num , count = 0;
+    printf("Please Enter The Number : ");
+   scanf("%d",&num);
+  if(num == 0){
+      count = 1;
+  }
+  while(num != 0){
+      num = (num /10);
+      count ++;
+  }
+  printf("The digits of number is : %d",count);
+}
